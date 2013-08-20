66653c664bc174cb3ff79e9964a6edea29fb21c6
Maurits van Rees <maurits@vanrees.org>
2013-08-20 15:29:41 +0200

    Note move to github.

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.8-dev'
+version = '0.8.dev0'
 
 setup(name='Products.Doormat',
       version=version,
       description="Adds a doormat viewlet and installs it in the Plone footer.  The links and text in the doormat are manageable as content.",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read() +
-                       open(os.path.join("docs", "DEVELOPERS.txt")).read() +
-                       open(os.path.join("docs", "TODO.txt")).read(),
+      long_description=(open("README.txt").read() + "\n" +
+                        open(os.path.join("docs", "HISTORY.txt")).read() +
+                        open(os.path.join("docs", "DEVELOPERS.txt")).read() +
+                        open(os.path.join("docs", "TODO.txt")).read()),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          ],
       keywords='',
       author='Kees Hink',
       author_email='hink@gw20e.com',
-      url='http://plone.org/products/doormat/',
+      url='https://github.com/collective/Products.doormat',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
@@ -27,7 +27,6 @@ setup(name='Products.Doormat',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
       ],
       entry_points="""
       # -*- Entry points: -*-
