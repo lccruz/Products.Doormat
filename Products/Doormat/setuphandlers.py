3839617eb7128b3a4f4e2d9a5ee0fc8aea296964
Kees Hink <keeshink@gmail.com>
2013-10-09 00:06:01 +0200

    Remove unused setuphandler methods

--- Products/Doormat/setuphandlers.py
+++ Products/Doormat/setuphandlers.py
@@ -8,20 +8,3 @@ logger = logging.getLogger('Doormat: setuphandlers')
 
 def isNotDoormatProfile(context):
     return context.readDataFile("Doormat_marker.txt") is None
-
-
-def updateRoleMappings(context):
-    """after workflow changed update the roles mapping. this is like pressing
-    the button 'Update Security Setting' and portal_workflow"""
-    if isNotDoormatProfile(context):
-        return
-    wft = getToolByName(context.getSite(), 'portal_workflow')
-    wft.updateRoleMappings()
-
-
-def postInstall(context):
-    """Called as at the end of the setup process. """
-    # the right place for your custom code
-    #if isNotDoormatProfile(context):
-    #    return
-    pass
