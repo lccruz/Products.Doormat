64becfb8bbb85a52ae76f50a885a8c1d87ab3142
Kees Hink <keeshink@gmail.com>
2013-10-10 01:32:38 +0200

    Create a column, section and page initially

--- Products/Doormat/setuphandlers.py
+++ Products/Doormat/setuphandlers.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 import logging
 
+DEFAULT_DOORMAT_DOCUMENT_HTML = """<p>
+This is the default doormat text.
+Please go to /doormat/column-1/section-1/document/edit to edit it.
+</p>"""
+DEFAULT_DOORMAT_DOCUMENT_TITLE = "Document title"
 logger = logging.getLogger('Doormat: setuphandlers')
 
 
@@ -10,6 +15,23 @@ def createDefaultContent(portal):
     doormat.setTitle('Doormat')
     doormat.setExcludeFromNav(True)  # Don't show in portal sections
     doormat.reindexObject()
+    doormat.invokeFactory('DoormatColumn', 'column-1')
+    column = getattr(doormat, 'column-1')
+    column.setTitle('Section 1')
+    column.reindexObject()
+    column.invokeFactory('DoormatSection', 'section-1')
+    section = getattr(column, 'section-1')
+    section.setTitle('Section 1')
+    section.reindexObject()
+    section.invokeFactory("Document", 'document-1')
+    document = getattr(section, 'document-1')
+    if document.meta_type.startswith('Dexterity'):
+        # A Dexterity-link
+        document.text = DEFAULT_DOORMAT_DOCUMENT_HTML
+        document.title = DEFAULT_DOORMAT_DOCUMENT_TITLE
+    else:
+        document.setText(DEFAULT_DOORMAT_DOCUMENT_HTML)
+        document.setTitle(DEFAULT_DOORMAT_DOCUMENT_TITLE)
 
 
 def isNotDoormatProfile(context):
